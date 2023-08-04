9904043decdc0356fea1ef9306ddf782fbdef8d9
ashu-tosh-patel <ashutoshpatelnoida@gmail.com>
2023-08-04 11:06:49 +0530

    implemented popup for login form

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,35 +1,45 @@
-import React from "react"
+import React,{useState} from "react";
 import { Link, useMatch } from "react-router-dom";
 import { FaSistrix } from "react-icons/fa";
-import './Header.css'
+import './Header.css';
 
+import LoginPopup from "./LoginPopup";
 import logo from '../assets/logo.png';
+
 const Header = () => {
-
-
-
     const exploreMatch = useMatch("/explore");
     const createMatch = useMatch("/create");
     const eventsMatch = useMatch("/events");
     const loginMatch = useMatch("/login");
     const signupMatch = useMatch("/signup");
 
-    return (
-        <nav className="navbar">
-            <ul>
-                {/* <li className="title">Osmosis learn</li> */}
-                <li className="title"><img src={logo} alt="logo"></img></li>
-                <>
-                    <li ><FaSistrix/></li>
-                    <li ><Link to="/explore" className={exploreMatch ? "active" : ""}>Explore</Link></li>
-                    <li ><Link to="/create" className={createMatch ? "active" : ""}>Create</Link></li>
-                    <li ><Link to="/events" className={eventsMatch ? "active" : ""}>Events</Link></li>
-                    <li ><Link to="/login" className={loginMatch ? "active" : ""}>Log In</Link></li>
-                    <li ><Link to="/signup" className={signupMatch ? "active" : ""}>Sign Up</Link></li>
-                </>
-            </ul>
-        </nav>
-    )
+  // State to handle the visibility of the login popup
+  const [showPopup, setShowPopup] = useState(false);
+
+  const openPopup = () => {
+    setShowPopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
+
+  return (
+    <nav className="navbar">
+      <ul>
+        <li className="title"><img src={logo} alt="logo"></img></li>
+        <>
+          <li><FaSistrix /></li>
+          <li><Link to="/explore" className={exploreMatch ? "active" : ""}>Explore</Link></li>
+          <li><Link to="/create" className={createMatch ? "active" : ""}>Create</Link></li>
+          <li><Link to="/events" className={eventsMatch ? "active" : ""}>Events</Link></li>
+          <li><button className="login-link" onClick={openPopup}>Log In</button></li>
+          <li><Link to="/signup" className={signupMatch ? "active" : ""}>Sign Up</Link></li>
+        </>
+      </ul>
+      {showPopup && <LoginPopup onClose={closePopup} />}
+    </nav>
+  );
 };
 
 export default Header;
--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,41 +1,30 @@
-import React from "react";
-import logo from '../assets/logo.png';
-import './login.css';
-import ggl from '../assets/google.png';    
-const login = () => {
-    return (
-        <div>
-            <h3>Login</h3>
-            <hr></hr>
-            <p>Not member yet? click <a href="null">here</a></p>
-            <div className="frm">
-            <span><img src={logo} alt="logo"></img></span>
-                <form>
-                    <label>
-                    <input className="inpt1" type="email" placeholder="Email" />
-                    </label>
-                    <label >
-                    <input className="inpt2" type="password" placeholder="Password" />
-                    </label>
-                    <div className="cptdiv">
-                    <div className="wcpt1">SAISW</div>
-                    <label>
-                    <div className="wcpt2">
-                    <input className="cpt" type="text" placeholder="Enter captcha" />
-                    </div>
-                    </label>
-                    </div>
-                    <div>
-                    <label>
-                    <input className="form-submit-button" type = "submit" value="Submit" />
-                    </label>
-                    </div>
-                </form>
-                <p>OR</p>
-                <p>Login with <span className="google"><img src={ggl} alt="google" className="ggle"></img></span></p>
-            </div>
-        </div>
-    )
-};
+// import React, { useState } from "react";
+// import LoginPopup from "./LoginPopup";
 
-export default login;
+// const Login = () => {
+//     const [showPopup, setShowPopup] = useState(false);
+
+//     const openPopup = () => {
+//         setShowPopup(true);
+//     };
+
+//     const closePopup = () => {
+//         setShowPopup(false);
+//     };
+
+//     return (
+//         <div>
+//             {/* <h3>Login</h3>
+//             <hr></hr> */}
+//             {/* <p>Not a member yet? click <a href="null">here</a></p>
+//       <div className="frm"> */}
+//             {/* ... rest of the login form code ... */}
+//             {/* <p>Login with <span className="google"><img src={ggl} alt="google" className="ggle"></img></span></p> */}
+//             {/* <button className="login-link" onClick={openPopup}>Log In</button> */}
+//             {/* </div> */}
+//             {showPopup && <LoginPopup onClose={closePopup} />}
+//         </div>
+//     );
+// };
+
+// export default Login;
